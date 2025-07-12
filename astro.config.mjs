dc4cb1ecd96a29555d6b3337528c4cbc8f677874
Shoyu <shuyong.yang@daolabs.com>
2025-07-12 15:00:32 +0200

    new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,33 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "PolinaOS v0.1 Alpha",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/polinaos",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Overview",
+          items: [
+            { label: "Introduction", slug: "introduction" },
+            { label: "Modules", slug: "modules" },
+            { label: "Workflow", slug: "workflow" },
+          ],
+        },
+        {
+          label: "Modules",
+          collapsed: true,
+          items: [],
+        },
+      ],
+    }),
+  ],
 });
