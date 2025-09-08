317e35aa61cd7f6da4d79c1350e8f1ee8e3cb5fa
degenshoyu <degenshushu@gmail.com>
2025-09-08 12:18:25 +0700

    improved all

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ export default defineConfig({
         {
           label: "Modules",
           collapsed: true,
-          items: [],
+          items: [{ label: "AI Modules", slug: "ai-core" }],
         },
       ],
     }),
